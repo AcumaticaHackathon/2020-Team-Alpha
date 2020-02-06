7298467988e0ee66a9ef35fb1d9446e2c3bab288
DSDNicole <44866033+DSDNicole@users.noreply.github.com>
2020-02-06 09:13:02 -0500

    Add files via upload

---
+++ ARInvoiceEntryExt.cs
@@ -0,0 +1,14 @@
+﻿using PX.Data;
+using PX.Objects.AR;
+
+namespace Cegid.Erp
+{
+    public class ARInvoiceEntryExt : PXGraphExtension<ARInvoiceEntry>
+    {
+        [PXMergeAttributes(Method = MergeMethod.Merge)]
+        [ARTranInventoryItem(Filterable = true)]
+        protected virtual void ARTran_InventoryID_CacheAttached(PXCache sender)
+        {
+        }
+    }
+}
---
+++ ARTranInventoryItemAttribute.cs
@@ -0,0 +1,36 @@
+﻿using PX.Data;
+using PX.Objects.AR;
+using PX.Objects.IN;
+
+namespace Cegid.Erp
+{
+    [PXDBInt]
+    [PXUIField(DisplayName = "Inventory ID", Visibility = PXUIVisibility.Visible)]
+    [PXRestrictor(typeof(Where<
+            Current<ARSetup.migrationMode>, Equal<True>,
+            Or<InventoryItem.stkItem, NotEqual<True>>>), PX.Objects.AP.Messages.CannotFindInventoryItem)]
+    [PXRestrictor(typeof(Where<
+            Current<ARSetup.migrationMode>, Equal<True>,
+            Or<Current<ARTran.sOOrderNbr>, IsNotNull,
+            Or<InventoryItem.stkItem, NotEqual<False>,
+            Or<InventoryItem.kitItem, NotEqual<True>>>>>), PX.Objects.SO.Messages.CannotAddNonStockKitDirectly)]
+    [PXRestrictor(typeof(Where<
+            Current<ARSetup.migrationMode>, Equal<True>,
+            Or<InventoryItem.stkItem, NotEqual<False>,
+            Or<InventoryItem.kitItem, NotEqual<True>>>>), PX.Objects.IN.Messages.CannotAddNonStockKit)]
+    [PXRestrictor(typeof(Where<InventoryItem.itemStatus, NotEqual<InventoryItemStatus.unknown>>), PX.Objects.PM.Messages.ReservedForProject)]
+    public class ARTranInventoryItemAttribute : InventoryAttribute
+    {
+        public ARTranInventoryItemAttribute()
+            : base(typeof(Search2<InventoryItem.inventoryID,
+                LeftJoin<UsrItemClassVisibility, On<UsrItemClassVisibility.itemClassID, Equal<InventoryItem.itemClassID>, And<UsrItemClassVisibility.bAccountID, Equal<Current<ARInvoice.customerID>>>>>,
+                Where2<Match<Current<AccessInfo.userName>>,
+                    And<Where<UsrItemClassVisibility.bAccountID, IsNotNull,
+                        Or<Not<Exists<Select<UsrItemClassVisibility,
+                            Where<UsrItemClassVisibility.bAccountID, Equal<Current<ARInvoice.customerID>>>>>>>>>>>),
+                typeof(InventoryItem.inventoryCD),
+                typeof(InventoryItem.descr))
+        {
+        }
+    }
+}
